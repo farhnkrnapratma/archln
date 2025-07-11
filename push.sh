70aef68b85181d8f0f8dfd02e86437093df610b9
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-07-11 20:33:10 +0700

    :: Update config(s) @[11-07-2025 20:07:S +0700]

--- push.sh
+++ push.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
+
+push_date=$(date +"%d-%m-%Y %H:%m:S %z")
+
 rm -rf nushell/history.txt
 git add .
-git commit -m "Update config(s)"
+git commit -m ":: Update config(s) @[$push_date]"
 git push
