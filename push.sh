0854d507976e490e7b0a0fb3cb12a8a57a782304
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-07-11 23:03:41 +0700

    [* main 983300d] Updating configuration @(11/07/2025 23:07:S +0700)

--- push.sh
+++ push.sh
@@ -2,8 +2,9 @@
 
 push_date=$(date +"%d/%m/%Y %H:%m:S %z")
 commit_hash=$(git rev-parse --short HEAD)
+branch_name=$(git branch)
 
 rm -rf nushell/history.txt
 git add .
-git commit -m "[$commit_hash] Updating configuration @($push_date)"
+git commit -m "[$branch_name $commit_hash] Updating configuration @($push_date)"
 git push
