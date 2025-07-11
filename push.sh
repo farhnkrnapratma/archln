ebf471cb0cf2eb88cce5e81c7970d637d86e1c79
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-07-11 21:43:43 +0700

    :: [b7780d6] Updating configuration <commit b7780d6e1ccaa72f99a72a5f00a125678197b78c Author: Farhan Kurnia Pratama <farhnkrnapratma@gmail.com> Date:   Fri Jul 11 21:38:46 2025 +0700
        :: [a7a8ea9] ^Updating configuration @(11/07/2025 21:07:S +0700)
    
     1 file changed, 3 insertions(+), 2 deletions(-)> @(11/07/2025 21:07:S +0700)

--- push.sh
+++ push.sh
@@ -2,8 +2,9 @@
 
 push_date=$(date +"%d/%m/%Y %H:%m:S %z")
 commit_hash=$(git rev-parse --short HEAD)
+git_stat=$(git show --shortstat $commit_hash)
 
 rm -rf nushell/history.txt
 git add .
-git commit -m ":: [$commit_hash] ^Updating configuration @($push_date)"
+git commit -m ":: [$commit_hash] Updating configuration <$git_stat> @($push_date)"
 git push
