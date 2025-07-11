cede4396772dc17db7d79a4e7f09e4d89f5047b1
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-07-11 21:46:49 +0700

    :: [ebf471c] Updating configuration <+ 43271 - 358> @(11/07/2025 21:07:S +0700)

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 push_date=$(date +"%d/%m/%Y %H:%m:S %z")
 commit_hash=$(git rev-parse --short HEAD)
-git_stat=$(git show --shortstat $commit_hash)
+git_stat=$(git log --pretty=tformat: --numstat | awk '{add+=$1; del+=$2} END {print "+", add, "-", del}')
 
 rm -rf nushell/history.txt
 git add .
